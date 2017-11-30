75a7e38d35c0a7be220e3b98266cd28708bcd135
Bastien Montois <oros.ogmius@gmail.com>
2017-11-30 16:05:34 +0100

    Update patch version

--- lib/yooda/version.rb
+++ lib/yooda/version.rb
@@ -3,7 +3,7 @@ module Yooda
   class Version
     MAJOR = 0 unless defined? Yooda::Version::MAJOR
     MINOR = 1 unless defined? Yooda::Version::MINOR
-    PATCH = 2 unless defined? Yooda::Version::PATCH
+    PATCH = 3 unless defined? Yooda::Version::PATCH
     PRE = nil unless defined? Yooda::Version::PRE
 
     class << self
